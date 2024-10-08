e035d608d0e3190d8a13592761d543a161ec36d4
GUILHERME ANDRADE SOUZA <guilherme.asouza29@senacsp.edu.br>
2024-10-07 21:25:31 -0300

    instalação do breeze, para tela de login e cadastro de usuário

--- CharlieDoces/routes/web.php
+++ CharlieDoces/routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProdutoController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/categoria',[CategoriaController::class, 'index']);
-Route::get('/categoria/{categoria}',[CategoriaController::class, 'show']);
-Route::get('/produtos',[ProdutoController::class, 'index']);
+Route::get('/categoria', [CategoriaController::class,'index']);
+Route::get('/categoria/{categoria}', [CategoriaController::class,'show']);//passado categoria como parâmetro 
+Route::get('/produtos', [ProdutoController::class,'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

cc2448246e1ac1ca9e20d9b172cbb7c5a678a00b
Rodrigo Holanda <105675993+Rodincrivel@users.noreply.github.com>
2024-11-12 15:29:11 -0300

    Footer e Header atualizados

--- CharlieDoces/app/Http/Controllers/HomeController.php
+++ CharlieDoces/app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Home;
 use App\Models\Produto;
 use App\Models\Categoria;
 
@@ -11,34 +10,35 @@ class HomeController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-       
+        // Carregar produtos e categorias específicas
         $produtos = Produto::all();
 
-       
+        // Exemplo de categorias específicas, você pode ajustar conforme necessário
         $categoriaChocolate = Categoria::with('produtos')->find(66);
-
         $categoriaNatal = Categoria::with('produtos')->find(84);
-
         $categoriaTopVendas = Categoria::with('produtos')->find(84);
 
-        
-        return view('home.index', ['produtos' => $produtos, 'categoriaChocolate' => $categoriaChocolate, 'categoriaNatal' => $categoriaNatal, 'categoriaTopVendas' => $categoriaTopVendas]);
-=======
-        return view('home.index', ['produtos' => Produto::All()]);
+        // Retornar a view com os dados necessários
+        return view('home.index', [
+            'produtos' => $produtos,
+            'categoriaChocolate' => $categoriaChocolate,
+            'categoriaNatal' => $categoriaNatal,
+            'categoriaTopVendas' => $categoriaTopVendas
+        ]);
     }
 
+    // Se precisar de outros métodos como para listar categorias:
     public function categoria()
     {
-        return view('home.index', ['categorias' => Categoria::All()]);
+        return view('home.index', ['categorias' => Categoria::all()]);
     }
 
+    // Método home para exibir produtos e categorias
     public function home()
     {
         return view('home.index', [
             'produtos' => Produto::all(),
             'categorias' => Categoria::all()
         ]);
->>>>>>> 4fb1552c8a6409db0076942619d502e1134b77c3
     }
 }

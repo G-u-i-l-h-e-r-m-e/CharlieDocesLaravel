5467f48fa0993f931322fd3ad01635961268b7ca
Rodrigo Holanda <105675993+Rodincrivel@users.noreply.github.com>
2024-11-13 09:53:57 -0300

    Update web.php
    update rotas

--- CharlieDoces/routes/web.php
+++ CharlieDoces/routes/web.php
@@ -1,17 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\CategoriaController;
-use App\Http\Controllers\ProdutoController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::group(['middleware' => ['auth']], function () {
+Route::middleware('auth')->group(function () {
     // Rotas para carrinho
     Route::get('carrinho/{produto}', [CarrinhoController::class, 'addCarrinho']);
     Route::get('carrinho', [CarrinhoController::class, 'carrinho']);
@@ -20,12 +18,12 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/produto/{produto}', [ProdutoController::class, 'show'])->name('produto.show');
     Route::get('/produtos', [ProdutoController::class, 'index']);
     Route::get('/todos_produtos', [ProdutoController::class, 'todosProdutos'])->name('produtos.todos');
-    
+
     // Rotas para categorias
     Route::get('/categoria', [CategoriaController::class, 'index']);
     Route::get('/categoria/{categoria}', [CategoriaController::class, 'show']);
-    
-    // Rota para perfil
+
+    // Rotas para perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -35,7 +33,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
 
-// Rota extra para login
+// Rota extra para login (fora do middleware de autenticação)
 Route::get('enviado', [LoginController::class, 'sent']);
 
+// Carregar rotas de autenticação padrão
 require __DIR__.'/auth.php';

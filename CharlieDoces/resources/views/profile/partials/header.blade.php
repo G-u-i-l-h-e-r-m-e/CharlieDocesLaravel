682ede5b69dd646c85c9f9fdf191fe2321bcaf50
Rodrigo Holanda <105675993+Rodincrivel@users.noreply.github.com>
2024-11-14 01:21:03 -0300

    Alterações no Header

--- CharlieDoces/resources/views/profile/partials/header.blade.php
+++ CharlieDoces/resources/views/profile/partials/header.blade.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Charlie Loja de Doces</title>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
 </head>
 
 <body>
     <header>
         <nav>
             <!-- Bloco 1: Linhas decorativas e logo -->
-            <div class="logo-section">
+            <div class="nav-top-section">
                 <span class="line"></span>
                 <a href="{{ url('/') }}">
                     <img src="{{ asset('img/header/logo.svg') }}" alt="Logo Charlie Loja de Doces">
@@ -22,58 +24,73 @@
             </div>
 
             <!-- Bloco 2: Campo de pesquisa e nome da loja -->
-            <div class="header-search">
+            <div class="nav-center-section">
                 <div class="search-section">
                     <div class="search-box">
                         <input type="text" placeholder="O QUE VOCÊ ESTÁ PROCURANDO?" class="search-input">
                         <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(89, 31, 18, 1);">
-                                <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                style="fill: rgba(89, 31, 18, 1);">
+                                <path
+                                    d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z">
+                                </path>
                             </svg>
                         </button>
                     </div>
 
-                    <img src="{{ asset('img/header/name-img.svg') }}" alt="Nome da Loja">
-                    <div>
-                        <p class="ney">neymar</p>
+                    <div class="store-name-arc">
+                        <svg width="293.1" height="59.41" viewBox="0 0 293.1 59.41">
+                            <!-- Caminho do arco ajustado -->
+                            <path id="arcPath" d="M0,59.41 Q146.55,30 293.1,59.41" fill="transparent" />
+                            <!-- Texto dentro do arco -->
+                            <text font-family="Bebas Neue" font-size="48px" fill="#591F12">
+                                <textPath href="#arcPath" startOffset="50%" text-anchor="middle">
+                                    LOJA DE DOCES
+                                </textPath>
+                            </text>
+                        </svg>
+                    </div>
+
+
+                    <!-- Logout, visível somente se o usuário estiver logado -->
+                    <div class="logout-section">
+                        <p class="logout-text">Logout</p>
                     </div>
                 </div>
             </div>
 
             <!-- Bloco 3: Navegação -->
-            <div class="nav-section">
-                <!-- Redireciona para página de produtos -->
-                <a href="{{ url('/todos_produtos') }}">Todos os Produtos</a>
+            <div class="nav-bottom-section">
+                <div class="nav-bottom-content">
+                    <!-- Redireciona para página de produtos -->
+                    <a href="{{ url('/todos_produtos') }}">Todos os Produtos</a>
 
-                <!-- Condicional para exibir 'Entre ou cadastre-se' ou saudação com o nome do usuário -->
-                @if (Auth::check())
-                    <a href="{{ url('/perfil') }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(89, 31, 18, 1);">
-                            <path d="M12 2A10.13 10.13 0 0 0 2 12a10 10 0 0 0 4 7.92V20h.1a9.7 9.7 0 0 0 11.8 0h.1v-.08A10 10 0 0 0 22 12 10.13 10.13 0 0 0 12 2zM8.07 18.93A3 3 0 0 1 11 16.57h2a3 3 0 0 1 2.93 2.36 7.75 7.75 0 0 1-7.86 0zm9.54-1.29A5 5 0 0 0 13 14.57h-2a5 5 0 0 0-4.61 3.07A8 8 0 0 1 4 12a8.1 8.1 0 0 1 8-8 8.1 8.1 0 0 1 8 8 8 8 0 0 1-2.39 5.64z"></path>
-                            <path d="M12 6a3.91 3.91 0 0 0-4 4 3.91 3.91 0 0 0 4 4 3.91 3.91 0 0 0 4-4 3.91 3.91 0 0 0-4-4zm0 6a1.91 1.91 0 0 1-2-2 1.91 1.91 0 0 1 2-2 1.91 1.91 0 0 1-2 2z"></path>
-                        </svg>
-                        Olá, {{ Auth::user()->name ?? 'Usuário' }}
-                    </a>
-                @else
-                    <a href="{{ url('/login') }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(89, 31, 18, 1);">
-                            <path d="M12 2A10.13 10.13 0 0 0 2 12a10 10 0 0 0 4 7.92V20h.1a9.7 9.7 0 0 0 11.8 0h.1v-.08A10 10 0 0 0 22 12 10.13 10.13 0 0 0 12 2zM8.07 18.93A3 3 0 0 1 11 16.57h2a3 3 0 0 1 2.93 2.36 7.75 7.75 0 0 1-7.86 0zm9.54-1.29A5 5 0 0 0 13 14.57h-2a5 5 0 0 0-4.61 3.07A8 8 0 0 1 4 12a8.1 8.1 0 0 1 8-8 8.1 8.1 0 0 1 8 8 8 8 0 0 1-2.39 5.64z"></path>
-                            <path d="M12 6a3.91 3.91 0 0 0-4 4 3.91 3.91 0 0 0 4 4 3.91 3.91 0 0 0 4-4 3.91 3.91 0 0 0-4-4zm0 6a1.91 1.91 0 0 1-2-2 1.91 1.91 0 0 1-2-2 1.91 1.91 0 0 1-2 2z"></path>
-                        </svg>
-                        Entre ou cadastre-se
-                    </a>
-                @endif
+                    <!-- Condicional para exibir 'Entre ou cadastre-se' ou saudação com o nome do usuário -->
+                    @if (Auth::check())
+                        <a href="{{ url('/perfil') }}">
+                            <i class="bx bx-user" style="color: #591f12;"></i>
+                            Olá, {{ Auth::user()->name ?? 'Usuário' }}
+                        </a>
+                    @else
+                        <a href="{{ url('/perfil') }}">
+                            <box-icon name="user-circle" color="#591f12" width="24px" height="24px"></box-icon>
+                            Olá, {{ Auth::user()->name ?? 'Usuário' }}
+                        </a>
+                    @endif
 
-                <!-- Verificação de login para acessar o carrinho -->
-                <a href="{{ Auth::check() ? url('/carrinho') : url('/login') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(89, 31, 18, 1);">
-                        <path d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8c.113-.302.06-.646-.135-.896zM9.248 15l-3.1-7.922.819-2.057h7.658l.857 2.057-3.099 7.922H9.248z"></path>
-                    </svg>
-                    Carrinho
-                </a>
+                    <!-- Ícone do carrinho -->
+                    <a href="{{ Auth::check() ? url('/carrinho') : url('/login') }}">
+                        <box-icon name="cart" color="#591f12" width="24px" height="24px"></box-icon>
+                        Carrinho
+                    </a>
+                </div>
             </div>
+
+
         </nav>
+
     </header>
+    <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
 </body>
 
 </html>
